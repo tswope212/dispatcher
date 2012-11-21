3b4cb5902c470dd7efe18aedb2ffce777d048ee9
Mike Schwab <mike.schwab@gmail.com>
2012-11-21 15:31:58 -0500

    typo

--- app/mailers/taylor_mailer.rb
+++ app/mailers/taylor_mailer.rb
@@ -3,6 +3,6 @@ class TaylorMailer < ActionMailer::Base
   
   def activation_authorization team_admin
     @team_admin = team_admin
-    mail :to => 'tswope212@gmail.com', :subject => 'New Team Admin'}
+    mail :to => 'tswope212@gmail.com', :subject => 'New Team Admin'
   end
 end
