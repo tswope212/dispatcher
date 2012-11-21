6e465c640d8bfb0070d5a7bea294bd6bea0b63a1
Mike Schwab <mike.schwab@gmail.com>
2012-11-21 15:29:25 -0500

    corrected activation mailer

--- app/mailers/taylor_mailer.rb
+++ app/mailers/taylor_mailer.rb
@@ -2,6 +2,7 @@ class TaylorMailer < ActionMailer::Base
   default from: "bot@disasterdispatcher.net"
   
   def activation_authorization team_admin
-    mail :to => 'tswope212@gmail.com', :subject => 'New Team Admin', :body => {:team_admin => team_admin}
+    @team_admin = team_admin
+    mail :to => 'tswope212@gmail.com', :subject => 'New Team Admin'}
   end
 end
