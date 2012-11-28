fd9eb5932885f62298c73d592617737824683b7a
Mike Schwab <mike.schwab@gmail.com>
2012-11-28 01:38:00 -0500

    use yellow layout for login page

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,9 @@ module Dispatcher
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.to_prepare do
+      Devise::SessionsController.layout "front" 
+    end
   end
 end
