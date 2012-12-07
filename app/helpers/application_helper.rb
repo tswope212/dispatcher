0a20c30836460371563ef3702a47da7f60d77f5a
Mike Schwab <mike.schwab@gmail.com>
2012-12-07 09:53:52 -0500

    jobs page protected from unit deletion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
-  def link_to_name obj
+  def link_to_name obj, method = nil
     if obj
-      link_to obj.name, obj rescue nil
+      if method
+        link_to obj.send(method), obj rescue nil
+      else
+        link_to obj.name, obj rescue nil
+      end
     end
   end
 
