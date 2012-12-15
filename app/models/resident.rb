da220aa2bb36e56d2a521f1f4b8d645c76b00281
Mike Schwab <mike.schwab@gmail.com>
2012-12-15 10:37:54 -0500

    use phone number for email

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -12,7 +12,16 @@ class Resident < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, "first_name", "last_name","primary_phone_number", "encrypted_password", "reset_password_token", "reset_password_sent_at", "remember_created_at", "sign_in_count", "current_sign_in_at", "last_sign_in_at", "current_sign_in_ip", "last_sign_in_ip", "confirmation_token","confirmed_at", "confirmation_sent_at", :date_of_birth, :signed_waiver, :unconfirmed_email, :authentication_token, :created_at, :updated_at
   
+  before_validation :use_phone_for_email_if_necessary, :on => :create
+  
   def name
     "#{first_name} #{last_name}"
+  end
+  
+  def use_phone_for_email_if_necessary
+    return true unless email.blank?
+    return false if primary_phone_number.blank?
+    phone_number_without_spaces = primary_phone_number.gsub(/\s/, '')
+    self.email = "#{phone_number_without_spaces}@disasterdispatcher.net"
   end
 end
