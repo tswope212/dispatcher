d9fc8792e4b698a1d42954a3c56ff47deb682b56
Mike Schwab <mike.schwab@gmail.com>
2012-12-16 17:13:15 -0500

    bucket names have dashes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AMAZON_ACCESS_KEY_ID'],
     :aws_secret_access_key  => ENV['AMAZON_ACCESS_KEY']
   }
-  config.fog_directory  = "dispatcher_#{Rails.env}"
+  config.fog_directory  = "dispatcher-#{Rails.env}"
 end
