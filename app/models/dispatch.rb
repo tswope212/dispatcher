01beee0b0b60dbb299742a8bffe296928141e9da
Mike Schwab <mike.schwab@gmail.com>
2012-12-19 20:27:17 -0500

    commented out all emails to team upon dispatch

--- app/models/dispatch.rb
+++ app/models/dispatch.rb
@@ -7,14 +7,14 @@ class Dispatch < ActiveRecord::Base
   scope :recent, :order => 'created_at desc'
   scope :incomplete, :joins => :job, :conditions => 'jobs.actual_end is null'
   
-  after_create :notify_team_lead, :notify_team_members
+  # after_create :notify_team_lead, :notify_team_members
   
   def name
     "dispatched to #{job.unit.full_address}"
   end
   
   def notify_team_lead
-    # TeamLeaderMailer.job_assigned_notification(self).deliver if team.person.andand.email.present?
+    TeamLeaderMailer.job_assigned_notification(self).deliver if team.person.andand.email.present?
   end
   
   def notify_team_members
