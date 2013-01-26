d76e54373dc705d4c00292658ff6a9a4b6bab567
Mike Schwab <mike.schwab@gmail.com>
2013-01-25 20:31:02 -0500

    fixed scope syntax

--- app/models/dispatch.rb
+++ app/models/dispatch.rb
@@ -5,10 +5,10 @@ class Dispatch < ActiveRecord::Base
   attr_accessible :team_id, :job_id
   
   scope :recent, order('created_at desc')
-  scope :incomplete, joins(:job), where('jobs.actual_end is null')
-  scope :unscheduled, joins(:job), where('jobs.scheduled_start is null')
-  scope :scheduled, joins(:job), where('jobs.scheduled_start is not null and jobs.actual_start is null')
-  scope :complete, joins(:job), where('jobs.actual_end is not null')
+  scope :incomplete, joins(:job).where('jobs.actual_end is null')
+  scope :unscheduled, joins(:job).where('jobs.scheduled_start is null')
+  scope :scheduled, joins(:job).where('jobs.scheduled_start is not null and jobs.actual_start is null')
+  scope :complete, joins(:job).where('jobs.actual_end is not null')
   
   after_create :notify_team_lead, :notify_team_members
   
